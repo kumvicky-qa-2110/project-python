cf818a0064aa5e1cddb30cb647190e24142a2aa2
kumar1.vicky <kumar1.vicky@paytm.com>
2026-04-28 03:46:12 +0000

    datatype code pushed

---
+++ python_programming/dataType.py
@@ -0,0 +1,56 @@
+# Data Type
+
+age = 35 #int
+name = "Vicky" #str
+cgpa = 8.0 #float
+
+# In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=).
+# Rule of defining a variable in Python  
+# Variable names must start with a letter (a-z, A-Z) or an underscore (_).
+# They can contain letters, numbers, and underscores.
+# Variable names are case-sensitive (age and Age are different).
+# Avoid using Python keywords (e.g., if, for, while) as variable names.
+
+# 34age = 4 # Invalid because variable cannot start with a number
+age = 34 # Valid because variable can start with a number 
+# a$$ge = 45 # Invlaid because variables cannot contain special characters other than _
+__age = 34
+__nice_45 = 34
+a_b_c_7 = "Sam"
+
+# printing a variable 
+
+age = 35
+print(age)
+print(type(age))
+
+name = "Vicky"
+print(name)
+print(type(name))
+
+cgpa = 8.50
+print(cgpa)
+print(type(cgpa))
+
+is_completed = True # boolean 
+print(is_completed)
+print(type(is_completed))
+
+
+# Python supports several built-in data types:
+#     Integers (int): Whole numbers (e.g., 10, -5).
+#     Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+#     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+#     Booleans (bool): Represents True or False.
+#     Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+#     Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+#     Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+#     Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+
+
+
+
+
+
+
