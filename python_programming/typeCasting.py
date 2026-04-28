5219309324c4cc4edafa24a9699d941e10f90991
kumar1.vicky <kumar1.vicky@paytm.com>
2026-04-28 04:15:45 +0000

    type casting code

---
+++ python_programming/typeCasting.py
@@ -0,0 +1,52 @@
+a = 30
+b = "35"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# converting string into integer
+
+c = int(b)
+print(c)
+print(type(c))
+
+# converting integer into string
+
+d = str(a)
+print(d)
+print(type(d))
+
+# exmaple 
+
+num_str = "10"
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
+
+num = 50
+num_str = str(num)
+print(num)
+print(type(num))
+
+pi = 3.14
+print(pi)
+print(type(pi))
+
+pi_str = str(pi)
+print(pi_str)
+print(type(pi_str))
+
+pi_num = int(pi)
+print(pi_num)
+print(type(pi_num))
+
+
+
+
+
+
+
+
