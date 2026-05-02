d6948dc3ae426d18429c901d879e2554480e24ad
kumvicky-qa-2110 <kumvicky.qa@gmail.com>
2026-05-02 11:01:54 +0000

    day 3 last merge

--- python_programming/operators.py
+++ python_programming/operators.py
@@ -1,3 +1,4 @@
+
 a = 34
 b = 2 
 
@@ -9,6 +10,69 @@ print("a / b = ", a / b)
 print("a % b = ", a % b)
 print("a // b = ", a // b)
 print("a ** b = ", a ** b)
+print("-------")
+
+# Conditinal Operator
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4)
+print(a==34)
+print(a!=4)
+print(a!=34)
+print("-------")
+
+# logical operators and
+
+c = True
+d = False
+print(c and c)
+print(c and d)
+print(d and c)
+print(d and d)
+print("-------")
+
+# logical operators or
+
+print(c or c)
+print(c or d)
+print(d or c)
+print(d or d)
+print("-------")
+
+# logical operators not
+
+print(not c)
+print(not d)
+print("-------")
+
+#logical operators assignment
+
+a = 10
+
+a += 5
+print(a)
+
+a -= 5
+print(a)
+
+a *= 5
+print(a)
+
+a /= 5
+print(a)    
+
+
+
+
+
+
+
+
+
+# 
+#  
 
 
  
---
+++ python_programming/practice_set.py
@@ -0,0 +1,19 @@
+# Practice Set
+
+# write a program that print Write a program that prints: Hello, World! Welcome to Python.
+
+print("Hello, World! Welcome to Python.")
+print("Hello",",","World!","Welcome","to","Python.")
+
+# Program 2
+
+name = "Vicky"
+age = 38
+height = 5.6
+student = True
+print("my name is",name,/n"my age is",age,"my height is",height,"I am a student",student,sep=",")
+
+
+
+
+
