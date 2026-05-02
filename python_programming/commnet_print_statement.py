78845fe4280f7420d6579bbb3ec945fc547c7d79
kumvicky-qa-2110 <kumvicky.qa@gmail.com>
2026-05-02 08:32:47 +0000

    day 3 merge 2

---
+++ python_programming/commnet_print_statement.py
@@ -0,0 +1,21 @@
+print ("Hi, I am using print statement in python programming language")
+
+#print ("I am learning python programming language")
+
+'''
+
+print ("I am learning python programming language")
+print ("I am enjoying learning python programming language")
+print ("I am learning python programming language")
+
+'''
+
+print("I am vicky \n I am 38 year old \n I am learning python programming")
+
+
+
+
+
+
+
+
---
+++ python_programming/multiply_two_number.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+print("The first number is: ", num1)
+
+num2 = float(input("Enter second number: "))
+print("The second number is: ", num2)
+
+mul = num1 * num2
+print("The multiplication of two numbers is: ", mul)
