7576e8cbe96df1f9ccba775a26cb3801b0d24f2a
kumvicky-qa-2110 <kumvicky.qa@gmail.com>
2026-05-02 09:01:09 +0000

    day 3 merge 3

--- python_programming/commnet_print_statement.py
+++ python_programming/commnet_print_statement.py
@@ -12,6 +12,13 @@ print ("I am learning python programming language")
 
 print("I am vicky \n I am 38 year old \n I am learning python programming")
 
+print("vicky \t 38 \t python programming")
+
+print("vicky","singh",38,sep="=")
+
+print("vicky","hunter",2020,end=",")
+print("royal enfield")
+
 
 
 
---
+++ python_programming/operators.py
@@ -0,0 +1,14 @@
+a = 34
+b = 2 
+
+# Arithmetic Operator 
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
+print("a // b = ", a // b)
+print("a ** b = ", a ** b)
+
+
+ 
